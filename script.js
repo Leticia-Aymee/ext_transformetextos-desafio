459b23df7474b96c97189efd8b7d3b3020d43826
Leticia-Aymee <leleaymee@gmail.com>
2025-10-27 08:44:32 -0300

    START, adicionando arquivos HTML, JSON, JS, CSS e pasta icons

---
+++ script.js
@@ -0,0 +1,43 @@
+// Programa - Modelo 1
+window.document.addEventListener("DOMContentLoaded", function () {
+  // Botão Letra Maiúscula
+  window.document
+    .querySelector("#btnMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.toUpperCase();
+    });
+
+  // Botão Letra Minúscula
+  window.document
+    .querySelector("#btnMinuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.toLowerCase();
+    });
+
+  // Botão Primeira Letra Maíuscula
+  window.document
+    .querySelector("#btnPriMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.charAt(0).toUpperCase() + inputText.slice(1);
+    });
+
+    // Botão Limpar
+    window.document
+    .querySelector("#btnLimpar")
+    .addEventListener("click", function () {
+      document.querySelector("#input-text").value = "";
+      document.querySelector("#result").innerHTML = "";
+    });
+
+
+    window.document
+    .querySelector("#btnDuplicar")
+    .addEventListener("click", function () {
+      let input = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerText = input + " " + input;
+    });
+
+});
