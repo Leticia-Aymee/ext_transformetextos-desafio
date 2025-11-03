1a66987ba140bf512f4169ebfb70d45039e808c7
Leticia-Aymee <leleaymee@gmail.com>
2025-11-03 09:42:37 -0300

    UPDATE: alteração do botão 'PrimeiraLetraMaiuscula' no arquivo de JS

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // Programa - Modelo 1
 window.document.addEventListener("DOMContentLoaded", function () {
+  let resultadoEl = document.querySelector("#result");
+
+  resultadoEl.innerHTML = "Resultado";
+  resultadoEl.style.color = "gray";
+
   // Botão Letra Maiúscula
   window.document
     .querySelector("#btnMaiuscula")
     .addEventListener("click", function () {
       let inputText = document.querySelector("#input-text").value;
-      document.querySelector("#result").innerHTML = inputText.toUpperCase();
+      resultadoEl.innerHTML = inputText.toUpperCase();
+      resultadoEl.style.color = "black";
     });
 
   // Botão Letra Minúscula
@@ -13,15 +19,23 @@ window.document.addEventListener("DOMContentLoaded", function () {
     .querySelector("#btnMinuscula")
     .addEventListener("click", function () {
       let inputText = document.querySelector("#input-text").value;
-      document.querySelector("#result").innerHTML = inputText.toLowerCase();
+      resultadoEl.innerHTML = inputText.toLowerCase();
+      resultadoEl.style.color = "black";
     });
 
   // Botão Primeira Letra Maíuscula
   window.document
     .querySelector("#btnPriMaiuscula")
     .addEventListener("click", function () {
-      let inputText = document.querySelector("#input-text").value;
-      document.querySelector("#result").innerHTML = inputText.charAt(0).toUpperCase() + inputText.slice(1);
+      let texto = document.querySelector("#input-text").value.toLowerCase();
+
+      let resultado = texto
+        .split(/(?<=[.!?])\s*/)
+        .map(frase => frase.charAt(0).toUpperCase() + frase.slice(1))
+        .join(" ");
+
+      resultadoEl.innerHTML = resultado;
+      resultadoEl.style.color = "black";
     });
 
     // Botão Limpar
@@ -29,15 +43,19 @@ window.document.addEventListener("DOMContentLoaded", function () {
     .querySelector("#btnLimpar")
     .addEventListener("click", function () {
       document.querySelector("#input-text").value = "";
-      document.querySelector("#result").innerHTML = "";
+      document.querySelector("#result").innerHTML = "Resultado";
+      resultadoEl.innerHTML = "Resultado";
+      resultadoEl.style.color = "gray";
     });
 
 
+    // Botão Duplicar
     window.document
     .querySelector("#btnDuplicar")
     .addEventListener("click", function () {
       let input = document.querySelector("#input-text").value;
-      document.querySelector("#result").innerText = input + " " + input;
+      resultadoEl.innerText = input + " " + input;
+      resultadoEl.style.color = "black";
     });
 
 });
